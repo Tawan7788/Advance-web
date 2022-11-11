ea866846a685319601ba6b816d710b739c17eaee
Tawan7788 <92264937+Tawan7788@users.noreply.github.com>
2022-11-11 21:45:17 +0700

    Add files via upload

---
+++ AdvanceWeb/AdvanceWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvanceWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
