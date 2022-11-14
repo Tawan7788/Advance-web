70e673e92e9010f41b6ddc6ae74dc39ce27143b5
Tawan7788 <masterkiler7788@gmail.com>
2022-11-14 11:47:28 +0700

    ALL API

---
+++ DemoWebAPIforstd/DemoWebAPIforstd/Data/IssueDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoWebAPIforstd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebAPIforstd.Data
+{
+    public class IssueDbContext: DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext>options): base(options)
+        {
+
+        }
+        public DbSet<Issue> Issues { get; set; }
+
+    }
+}
